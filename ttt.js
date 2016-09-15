cb8f9cd37505b75e97c5a3e527b7f9f0bb223179
Your Name <arssonist@yahoo.com>
2016-09-14 20:15:14 -0400

    x and o board working but not game over

--- ttt.js
+++ ttt.js
@@ -1,6 +1,45 @@
-$ (document).on("ready", function() {
 
+
+var turn = 0;
+
+
+
+$(document).ready (function() {
+
+// this refers to class/element/id above
   $('td').click(function(){
-    $('td').text('X');
-  })
-})
+
+    if ($(this).text() === '') {
+      if(turn % 2 === 0){
+        $(this).text('X');
+      } else {
+        $(this).text('O');
+      }
+    }
+
+    $( '.column1' ).each( function() {
+      
+    } );
+
+
+
+    turn++;
+  });
+});
+
+
+
+
+// text() is empty text
+// if ($('td').text() === '') {
+//   $(".status").text("Hey, who turned off the lights?");
+//   }
+// xo_array = ["x","o","x","o","x","o","x","o","x"];
+//
+// $('td').click(function() {
+//   if (turn % 2 === 0)
+// } ##make even
+// } else if  (turn % 2 === !0)
+//   ## make odd
+// }
+//   }
